f9ec89bdc3bbedcd4e7f14a0c981f1676ff45f07
LJosueCM <ljosuecm@gmail.com>
2022-04-27 15:22:17 -0500

    18) ExplorerController pruebas instanciadas

---
+++ test/services/ExplorerController.test.js
@@ -0,0 +1,18 @@
+const ExplorerController = require("./../../lib/controllers/ExplorerController");
+
+describe("Explorer Controller tests",()=>{
+    test("1) Get a list of explorers in a mission", () => {
+        const explorersInMission = ExplorerController.getExplorersByMission("node");
+        expect(explorersInMission.length).toBe(10);
+    });
+
+    test("2) Get a username list of explorers in a mission", () => {
+        const explorersByUsernameInMission = ExplorerController.getExplorersUsernamesByMission("node");
+        expect(explorersByUsernameInMission).toContain("ajolonauta5");
+    });
+    
+    test("3) Get the amount of explorers in a mission", () => {
+        const explorersInMissionAmount = ExplorerController.getExplorersAmonutByMission("node");
+        expect(explorersInMissionAmount).toBe(10);
+    });
+});
